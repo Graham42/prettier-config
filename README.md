d9b2f2cbc5496f479b724a800bdbf65481438587
Graham McGregor <graham@gmcgregor.ca>
2018-06-09 15:49:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Prettier Config
+
+[Prettier](https://prettier.io) is an awesome tool, however I have a preference
+for some config that differs from the defaults. My reasoning is fully explained
+in the [config file](./index.js).
---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  /*
+   * Requiring trailing commas makes git history work better. If you have an
+   * object:
+   * {
+   *   a: 1,
+   *   b: 2
+   * }
+   * and then add a property after 'b':
+   * {
+   *   a: 1,
+   *   b: 2,
+   *   c: 3
+   * }
+   * the line with 'b' gets changed (it now requires a comma) even though
+   * there's no real meaningful change to it.
+   *
+   * When using the `git blame` command, git looks at line by line changes.
+   * These comma only changes make it more difficult to follow history.
+   */
+  trailingComma: "all",
+  /**
+   * When viewing a markdown file with less, cat, vim, etc. the document is
+   * much easier to read if the text is hard wrapped at 80 characters.
+   *
+   * This is a tradeoff because changes will often result in git diffs that
+   * have changes across multiple lines. This can be solved by using the
+   * --word-diff option.
+   */
+  proseWrap: "always",
+};
