426ae023e70e80fc6dd8ce0e39358bb78aa28400
Graham McGregor <graham@gmcgregor.ca>
2018-12-07 11:02:36 -0500

    Simplify es5 config file

--- es5.js
+++ es5.js
@@ -3,12 +3,10 @@ const base = require("./index");
 /**
  * @type { import("prettier").Options }
  */
-const es5 = {};
-for (var nextKey in base) {
-  // @ts-ignore
-  es5[nextKey] = base[nextKey];
-}
-// If we're writing es5 code the base config of "all" will cause syntax errors
-es5.trailingComma = "es5";
+const es5 = {
+  ...base,
+  // If we're writing es5 code the base config of "all" will cause syntax errors
+  trailingComma: "es5",
+};
 
 module.exports = es5;
