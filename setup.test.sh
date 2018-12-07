48d7866cf835700fe567e8cbdda1b0a03af1e5da
Graham McGregor <graham@gmcgregor.ca>
2018-12-07 12:18:39 -0500

    Add basic test

---
+++ setup.test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -exo pipefail;
+
+ORIG_DIR=$(pwd)
+function finish {
+    cd "$ORIG_DIR"
+}
+trap finish EXIT
+
+cd "$(mktemp -d)"
+npm init -y
+npx $ORIG_DIR
+ls -a
+
+echo "'prettier.config.js' should exist"
+[ -e "prettier.config.js" ]
+
+echo "'.prettierignore' should exist"
+[ -e ".prettierignore" ]
+
+echo "format script should exist"
+npm run format
+
+echo "checkFormat script should exist"
+npm run checkFormat
