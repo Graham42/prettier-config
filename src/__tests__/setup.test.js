7168e8e1df2d0c415deac2b964bb9eaef516f67b
Graham McGregor <graham@gmcgregor.ca>
2022-05-25 12:41:00 -0400

    chore: remove dead comment

--- src/__tests__/setup.test.js
+++ src/__tests__/setup.test.js
@@ -1,30 +1,5 @@
 //@ts-check
 
-// ORIG_DIR=$(pwd)
-// function finish {
-//     cd "$ORIG_DIR"
-//     echo "Results in $TEMP_DIR"
-// }
-// trap finish EXIT
-
-// TEMP_DIR="$(mktemp -d)"
-// cd "$TEMP_DIR"
-// npm init -y
-// npx "$ORIG_DIR"
-// ls -a
-
-// echo "'.prettierrc.js' should exist"
-// [ -e ".prettierrc.js" ]
-
-// echo "'.gitignore' should exist"
-// [ -e ".gitignore" ]
-
-// echo "check:format script should exist"
-// npm run check:format
-
-// echo "fix:format script should exist"
-// npm run fix:format
-
 const { existsSync } = require("fs");
 const fsPromises = require("fs/promises");
 const os = require("os");
